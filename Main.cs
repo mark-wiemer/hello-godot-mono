7bd675b57bc859bcbee0884a90a991e7d5d6b60b
Mark Wiemer <markwiemer@outlook.com>
2024-12-30 20:43:01 -0800

    Add some code, basic score update on start

--- Main.cs
+++ Main.cs
@@ -3,16 +3,42 @@ using System;
 
 public partial class Main : Node
 {
-	static int score = 0;
-	static bool gameStarted = false;
+	[Export]
+	public PackedScene SnakeSegment;
+	
+	// Game variables
+	private int _score = 0;
+	private bool _gameStarted = false;
+	
+	// Grid variables
 	// Number of cells in each row and column of the grid
-	static int gridSize = 20;
+	private readonly int _gridSize = 20;
 	// Size of each cell in pixels
-	static int cellSize = 50;
+	private readonly int _cellSize = 50;
+	
+	// Snake variables
+	Vector2[] oldData;
+	Vector2[] newData;
+	Vector2[] snake; 
+	
+	// Movement variables
+	Vector2 startPos = new Vector2(9, 9);
+	Vector2 up = new Vector2(0, -1);
+	Vector2 down = new Vector2(0, 1);
+	Vector2 left = new Vector2(-1, 0);
+	Vector2 right = new Vector2(1, 0);
+	Vector2 direction;
+	bool canMove;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		NewGame();
+	}
+
+	private void NewGame() {
+		_score = 10;
+		GetNode("HUD").GetNode<Label>("Score").Text = "Score: " + _score;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
